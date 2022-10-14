1ea408346920b3117f7de36e48b2c9af86f32f4c
satishperla <47620285+satishperla@users.noreply.github.com>
2022-10-14 12:08:04 +0530

    Add files via upload

--- 13_pinGuess.js
+++ 13_pinGuess.js
@@ -10,3 +10,17 @@ function pinGuess(pin,arr){
 }
 
 pinGuess(0704,[4544,4444,0704,3467]);
+
+
+//Using prompt method
+
+var pin= '0704'
+for (var i=0; i<4;i++){
+    var arr= prompt("Please enter pin",)
+    if(arr==pin){
+        console.log("That is correct guess");
+        break;
+    }
+    else
+    console.log("That is wrong guess");
+}
